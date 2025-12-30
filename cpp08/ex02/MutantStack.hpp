7be858e0ef55bf3e79870286418c31791cbf5b56
Nassim Kalkoul <nkalkoul@f1br7s17.clusters.42paris.fr>
2025-12-30 11:01:53 +0100

    ok

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,17 +6,47 @@
 /*   By: nkalkoul <nkalkoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 13:35:55 by nkalkoul          #+#    #+#             */
-/*   Updated: 2025/12/29 14:15:17 by nkalkoul         ###   ########.fr       */
+/*   Updated: 2025/12/30 10:00:12 by nkalkoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <stack>
+#include <iostream>
+#include <deque>
 
 template <typename T>
-class MutantStack : public std::stack<T>{
-	
+class MutantStack : public std::stack<T> {
+
 public:
-	typedef std::stack<T>::iterator it;
+	typedef typename std::stack<T>::container_type::iterator	iterator;
+	typedef typename std::stack<T>::container_type::const_iterator	const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator	reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+
+	iterator begin(void){
+		return (this->c.begin());
+	}
+	iterator end(void){
+		return (this->c.end());
+	}
+	const_iterator cbegin(void) const{
+		return (this->c.begin());
+	}
+	const_iterator cend(void) const{
+		return (this->c.end());
+	}
+	reverse_iterator rbegin(void) {
+		return (this->c.rbegin());
+	}
+	reverse_iterator rend(void){
+		return (this->c.rend());
+	}
+	const_reverse_iterator crbegin(void) const{
+		return (this->c.rbegin());
+	}
+	const_reverse_iterator crend(void) const{
+		return (this->c.rend());
+	}
 };
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,9 +6,81 @@
 /*   By: nkalkoul <nkalkoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 13:41:30 by nkalkoul          #+#    #+#             */
-/*   Updated: 2025/12/23 13:41:38 by nkalkoul         ###   ########.fr       */
+/*   Updated: 2025/12/30 10:59:25 by nkalkoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
 
+#include "MutantStack.hpp"
+#include <list>
+
+int main()
+{
+	std::cout << "---------------STACK---------------\n" << std::endl;
+	
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	std::cout << "\nm stack with iterator :\n" << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	MutantStack<int>::reverse_iterator itee = mstack.rbegin();
+	MutantStack<int>::reverse_iterator iteee = mstack.rend();
+	std::cout << "\nm stack with reverse iterator :\n" << std::endl;
+	while (itee != iteee)
+	{
+		std::cout << *itee << std::endl;
+		++itee;
+	}
+
+	std::cout << "\n\n---------------LIST---------------\n" << std::endl;
+	
+	std::list<int> slist;
+	slist.push_front(5);
+	slist.push_front(17);
+	std::cout << slist.front() << std::endl;
+	slist.pop_front();
+	std::cout << slist.size() << std::endl;
+	slist.push_front(3);
+	slist.push_front(5);
+	slist.push_front(737);
+	//[...]
+	slist.push_front(0);
+	std::list<int>::iterator i = slist.begin();
+	std::list<int>::iterator ie = slist.end();
+	++it;
+	--it;
+	std::cout << "\ns LIST with iterator :\n" << std::endl;
+	while (i != ie)
+	{
+		std::cout << *i << std::endl;
+		++i;
+	}
+	std::list<int>::reverse_iterator a = slist.rbegin();
+	std::list<int>::reverse_iterator ae = slist.rend();
+	std::cout << "\ns LIST with reverse iterator :\n" << std::endl;
+	while (a != ae)
+	{
+		std::cout << *a << std::endl;
+		++a;
+	}
+
+	
+	return 0;
+}
